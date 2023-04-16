976bb6c1451cddc392d2142d0af77f6eb0186c0e
Tom <87443371+IceTomPlayz@users.noreply.github.com>
2023-04-16 07:37:13 -0400

    corley has no dad
    fr bro

---
+++ source/ExtrasState.hx
@@ -0,0 +1,120 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.system.FlxSound;
+
+using StringTools;
+
+class ExtrasState extends MusicBeatState
+{
+	var options:Array<String> = [
+		'Election',
+		'Baked',
+		'Blunder'
+	];
+	private var grpTexts:FlxTypedGroup<Alphabet>;
+
+	private var curSelected = 0;
+
+	override function create()
+	{
+		FlxG.camera.bgColor = FlxColor.BLACK;
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("Editors Main Menu", null);
+		#end
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.scrollFactor.set();
+		bg.color = 0xFFFFFFFF;
+		add(bg);
+
+		grpTexts = new FlxTypedGroup<Alphabet>();
+		add(grpTexts);
+
+		for (i in 0...options.length)
+		{
+			var leText:Alphabet = new Alphabet(90, 320, options[i], true);
+			leText.isMenuItem = true;
+			leText.targetY = i;
+			grpTexts.add(leText);
+			leText.snapToPosition();
+		}
+
+		changeSelection();
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.UI_UP_P)
+		{
+			changeSelection(-1);
+		}
+		if (controls.UI_DOWN_P)
+		{
+			changeSelection(1);
+		}
+
+		if (controls.BACK)
+		{
+			MusicBeatState.switchState(new MainMenuState());
+		}
+
+		if (controls.ACCEPT)
+		{
+			switch(options[curSelected]) {
+				case 'Election':
+					PlayState.SONG = Song.loadFromJson('election', 'election');
+                    LoadingState.loadAndSwitchState(new PlayState());
+				case 'Baked':
+					PlayState.SONG = Song.loadFromJson('baked', 'baked');
+                    LoadingState.loadAndSwitchState(new PlayState());
+				case 'Blunder':
+					PlayState.SONG = Song.loadFromJson('blunder', 'blunder');
+                    LoadingState.loadAndSwitchState(new PlayState());
+			}
+			FlxG.sound.music.volume = 0;
+			FreeplayState.destroyFreeplayVocals();
+		}
+		
+		var bullShit:Int = 0;
+		for (item in grpTexts.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			// item.setGraphicSize(Std.int(item.width * 0.8));
+
+			if (item.targetY == 0)
+			{
+				item.alpha = 1;
+				// item.setGraphicSize(Std.int(item.width));
+			}
+		}
+		super.update(elapsed);
+	}
+
+	function changeSelection(change:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		curSelected += change;
+
+		if (curSelected < 0)
+			curSelected = options.length - 1;
+		if (curSelected >= options.length)
+			curSelected = 0;
+	}
+}
